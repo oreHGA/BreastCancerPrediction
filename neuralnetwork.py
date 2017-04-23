35bc50ff58d2d12fd5c038ae4c49a6f85cd94c4f
oreHGA <oreogundipe@gmail.com>
2017-04-23 16:21:19 -0700

    created starter files for neural networks and svm

---
+++ neuralnetwork.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy
+import sklearn as scikit
+import sklearn.preprocessing
+
+def main():
+    location = "breastcancerdataset.csv"
+    dataset = pd.read_csv(location, ',')
+    labels = dataset.iloc[:, 1].values 
+    features = dataset.iloc[:, 2:].values
+
+    # now we need to perform feature scaling on the features 
+    features = scikit.preprocessing.normalize(features,'l1')
+    train_x = features[0:470]
+    train_y = labels[0:470]
+    test_x = features[471: , ]
+    test_y = labels[471: ]
+
+    print test_y
+
+
+main()
