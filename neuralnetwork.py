f10b1bcf532bb725baa044b4ea330640cc792d9f
Roberto Perez Soriano <robertoperez07@users.noreply.github.com>
2017-04-23 18:13:59 -0700

    Add files via upload
    Updated neuralnetwork.py file

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -3,10 +3,13 @@ import numpy
 import sklearn as scikit
 import sklearn.preprocessing
 
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report,confusion_matrix
+
 def main():
     location = "breastcancerdataset.csv"
     dataset = pd.read_csv(location, ',')
-    labels = dataset.iloc[:, 1].values 
+    labels = dataset.iloc[:, 1].values
     temp = labels
     count = 0
     for i in labels:
@@ -17,14 +20,23 @@ def main():
         count = count + 1
     features = dataset.iloc[:, 2:].values
 
-    # now we need to perform feature scaling on the features 
+    # now we need to perform feature scaling on the features
     features = scikit.preprocessing.normalize(features,'l1')
     train_x = features[0:470, ]
     train_y = temp[0:470]
     test_x = features[470: , ]
     test_y = temp[470: ]
 
-    print test_y
+    clf = MLPClassifier(alpha=1e-5, hidden_layer_sizes=(30,30,30))
+
+    clf.fit(train_x,train_y)
+
+    predictions = clf.predict(test_x)
+
+    print(confusion_matrix(test_y,predictions))
+    print(classification_report(test_y,predictions))
+
+    # print test_y
 
 
 main()
