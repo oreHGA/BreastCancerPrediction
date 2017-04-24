9e0a8bfc5ab0a8a246242c5f9d1ef145d8475d0c
oreHGA <oreogundipe@gmail.com>
2017-04-23 17:08:03 -0700

    Changed our y's to be 0 if Malignant and 1 if Benign

--- logisticregression.py
+++ logisticregression.py
@@ -7,14 +7,22 @@ def main():
     location = "breastcancerdataset.csv"
     dataset = pd.read_csv(location, ',')
     labels = dataset.iloc[:, 1].values 
+    temp = labels
+    count = 0
+    for i in labels:
+        if (i=='M'):
+            temp[count] = 0
+        else:
+            temp[count] = 1
+        count = count + 1
     features = dataset.iloc[:, 2:].values
 
     # now we need to perform feature scaling on the features 
     features = scikit.preprocessing.normalize(features,'l1')
     train_x = features[0:470, ]
-    train_y = labels[0:470]
+    train_y = temp[0:470]
     test_x = features[470: , ]
-    test_y = labels[470: ]
+    test_y = temp[470: ]
 
     print test_y
 
