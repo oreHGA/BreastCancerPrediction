001cf131a9b416b9804caefb77498e09d3a67c79
dakshina <dakpoojar@gmail.com>
2017-04-23 17:44:55 -0700

    Update svm.py
    updated with svm model fit

--- svm.py
+++ svm.py
@@ -1,7 +1,10 @@
 import pandas as pd
-import numpy
+import numpy as np
 import sklearn as scikit
 import sklearn.preprocessing
+from sklearn import svm
+from sklearn import metrics 
+import sklearn.utils.multiclass as checking
 
 def main():
     location = "breastcancerdataset.csv"
@@ -9,13 +12,15 @@ def main():
     labels = dataset.iloc[:, 1].values 
     temp = labels
     count = 0
+   
     for i in labels:
         if (i=='M'):
-            temp[count] = 1
+            temp[count] = '1'
         else:
-            temp[count] = 0
+            temp[count] = '0'
         count = count + 1
     features = dataset.iloc[:, 2:].values
+    
 
     # now we need to perform feature scaling on the features 
     features = scikit.preprocessing.normalize(features,'l1')
@@ -23,7 +28,12 @@ def main():
     train_y = temp[0:470]
     test_x = features[470: , ]
     test_y = temp[470: ]
-    print test_y
+    #print(checking.check_classification_targets(test_y))
+    model = svm.SVC()
+    model.fit(train_x,train_y)
+    prediction=model.predict(test_x)
+    y = metrics.accuracy_score(test_y,prediction)
+    print(y)
 
 
 main()
