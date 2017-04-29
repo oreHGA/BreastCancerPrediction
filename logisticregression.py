3873dcd3ce25f7709e6892567538b34d7eee4ccb
oreHGA <oreogundipe@gmail.com>
2017-04-29 04:05:34 -0700

    Better formatted code..on to evaluation & plotting

--- logisticregression.py
+++ logisticregression.py
@@ -9,9 +9,9 @@ from sklearn import metrics
 def plotfeatures(xsample, ysample):
     count = 0
     for i in ysample:
-        if i == '1':
+        if i == 'M':
             malignant, = plt.plot(xsample[count, 1], xsample[count, 2], 'ro')
-        elif i == '0':
+        elif i == 'B':
             benign, = plt.plot(xsample[count, 1], xsample[count, 2], 'bo')
         plt.hold(True)
         count += 1
@@ -25,21 +25,23 @@ def getdataset(location="./data/breastcancerdataset.csv"):
     dataset = pd.read_csv(location, ',')
     labels = dataset.iloc[:, 1].values
     count = 0
+    temp = []
     for i in labels:
         if i == 'M':
-            i = '1'
+            temp.append('1')
         else:
-            i = '0'
+            temp.append('0')
         count = count + 1
     features = dataset.iloc[:, 2:].values
     features = sklearn.preprocessing.normalize(features, 'l1')
     train_x = features[0:235, ]
-    train_y = labels[0:235]
+    train_y = temp[0:235]
     valid_x = features[235:470, ]
-    valid_y = labels[235:470]
+    valid_y = temp[235:470]
     test_x = features[470:, ]   # we do not touch these variables until the we get the best model
-    test_y = labels[470:]       # we do not touch these variables until the we get the best model
-    #plotfeatures(xsample=features, ysample=labels)
+    test_y = temp[470:]       # we do not touch these variables until the we get the best model
+    plotfeatures(xsample=features, ysample=labels)
+    print temp
     return train_x, train_y, valid_x, valid_y, test_x, test_y
 
 def logistic():
@@ -54,20 +56,11 @@ def logistic():
     model_b = model_b.fit(train_x, train_y)
     prediction = model_b.predict(valid_x)
     accuracy_b = metrics.accuracy_score(valid_y, prediction)
-
     if accuracy_a > accuracy_b:
-        print "A is more accurate"
-        print accuracy_a
+        better_model = model_a
     else:
-        print "B is more accurate"
-        print accuracy_b
-
-
-    # we'll do this after getting the best model
-    #conf_matrix_A = metrics.confusion_matrix(test_y, prediction)
-    #eval_A = metrics.classification_report(test_y, prediction)
-    # now we generate the confusion matrix
-    #  next step is to start plotting the graphs showing the error rates
+        better_model = model_b
+    return better_model
 
 logistic()
 
